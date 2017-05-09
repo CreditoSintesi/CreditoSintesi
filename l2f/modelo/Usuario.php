f8a2ccff523aa4c59cc57ffc084717cd63d875a5
MarcPetit92 <1001489.joan23@fje.edu>
2017-05-09 16:16:20 +0200

    actualizo modelo usuario

--- l2f/modelo/Usuario.php
+++ l2f/modelo/Usuario.php
@@ -5,11 +5,90 @@ class Usuario {
 
     
     //atributos
+    private $id_usuario;
     private $nombre_usuario;
     private $email_usuario;
     private $pass_usuario;
+    private $apellidos_usuario;
+    private $sexo_usuario;
+    private $fecha_nacimiento;
+    private $altura_usuario;
+    private $peso_usuario;
     
-    //constructor
+    
+    function getNombre_usuario() {
+        return $this->nombre_usuario;
+    }
+
+    function getEmail_usuario() {
+        return $this->email_usuario;
+    }
+
+    function getPass_usuario() {
+        return $this->pass_usuario;
+    }
+
+    function getApellidos_usuario() {
+        return $this->apellidos_usuario;
+    }
+
+    function getSexo_usuario() {
+        return $this->sexo_usuario;
+    }
+
+    function getFecha_nacimiento() {
+        return $this->fecha_nacimiento;
+    }
+
+    function getAltura_usuario() {
+        return $this->altura_usuario;
+    }
+
+    function getPeso_usuario() {
+        return $this->peso_usuario;
+    }
+
+    function setNombre_usuario($nombre_usuario) {
+        $this->nombre_usuario = $nombre_usuario;
+    }
+
+    function setEmail_usuario($email_usuario) {
+        $this->email_usuario = $email_usuario;
+    }
+
+    function setPass_usuario($pass_usuario) {
+        $this->pass_usuario = $pass_usuario;
+    }
+
+    function setApellidos_usuario($apellidos_usuario) {
+        $this->apellidos_usuario = $apellidos_usuario;
+    }
+
+    function setSexo_usuario($sexo_usuario) {
+        $this->sexo_usuario = $sexo_usuario;
+    }
+
+    function setFecha_nacimiento($fecha_nacimiento) {
+        $this->fecha_nacimiento = $fecha_nacimiento;
+    }
+
+    function setAltura_usuario($altura_usuario) {
+        $this->altura_usuario = $altura_usuario;
+    }
+
+    function setPeso_usuario($peso_usuario) {
+        $this->peso_usuario = $peso_usuario;
+    }
+    
+    public function Usuario($apellidos_usuario ,$peso_usuario, $altura_usuario, $fecha_nacimiento, $sexo_usuario ){
+        
+        $this->apellidos_usuario = $apellidos_usuario;
+        $this->fecha_nacimiento = $fecha_nacimiento;
+        $this->peso_usuario = $peso_usuario;
+        $this->sexo_usuario = $sexo_usuario;
+        $this->altura_usuario = $altura_usuario;
+    }
+        //constructor
     public function __construct($nombre_usuario, $email_usuario, $pass_usuario) {
         $this->nombre_usuario = $nombre_usuario;
         $this->email_usuario = $email_usuario;
@@ -20,47 +99,6 @@ class Usuario {
     
   
 
-    //getters
-    public function getNombreUsuario() {
-        return $this->nombre_usuario;
-    }
-
-    public function getEmailUsuario() {
-        return $this->email_usuario;
-    }
-
-    public function getPassUsuario() {
-        return $this->pass_usuario;
-    }
-
-    public function getFechaRegistro() {
-        return $this->fecha_registro;
-    }
-
-    public function getEdadUsuario() {
-        return $this->edad_usuario;
-    }
-
-    //setters
-    public function setNombreUsuario($nombre) {
-        $this->nombre_usuario = $nombre_usuario;
-    }
-
-    public function setEmailUsuario($email) {
-        $this->email_usuario = $email_usuario;
-    }
-
-    public function setPassUsuario($pass) {
-        $this->pass_usuario = $pass_usuario;
-    }
-
-    public function setFechaRegistro($fecha_registro) {
-        $this->fecha_registro = $fecha_registro;
-    }
-
-    public function setEdadUsuario($edad) {
-        $this->edad_usuario = $edad;
-    }
 
     public function alta(Usuario $usuario) {
         try {
@@ -88,38 +126,43 @@ class Usuario {
             //$ejecutar->execute(array('nombre_usuario'=> $usuario->getNombreUsuario(),'pass_usuario'=> $usuario->getPassUsuario(),'email_usuario'=> $usuario->getEmailUsuario() ));
             //ejecutamos la sentencia sql
             $ejecutar->execute();
+            
+            //
+            
             //$resultado = $ejecutar->fetchAll();
             
         } catch (Exception $ex) {
             echo $ex->getMessage();
         }
     }
-        public function updateActualizar(Usuario $usuario) {
+        public function updateCuestionario(Usuario $usuario) {
         try {
             //hacemos los getters del objeto usuario
-            $nombre_usuario = $usuario->getNombreUsuario();
-            $email_usuario = $usuario->getEmailUsuario();
-            $pass_usuario = $usuario->getPassUsuario();     
-            $fecha_registro=date("Y/m/d");
+                
+            $fecha_update=date("Y/m/d");
             
             //iniciamos una nueva conexion PDO a nuestra bd
             $conexion = new PDO('mysql:host=localhost; dbname=bd_l2f', 'root', '');
             $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             //generamos nuestra sentencia sql con los valores del insert y las variables
             
-            //$sql = "INSERT INTO `tbl_usuario` (`nombre_usuario`, `email_usuario`, `pass_usuario`) VALUES (:nombre_usuario, :email_usuario, :pass_usuario)";
-            $sql = "INSERT INTO tbl_usuario (nombre_usuario, email_usuario, pass_usuario, fecha_registro) VALUES (:nombre_usuario, :email_usuario, :pass_usuario, :fecha_registro)";
-            
+           
+             $sql = "UPDATE `tbl_usuario`   
+                    (`apellidos_usuario`,`sexo_usuario`,`altura_usuario`,`fecha_nacimiento`) 
+                    VALUES (:apellidos_usuario, :sexo_usuario, :altura_usuario, :fecha_nacimiento)
+                    WHERE tbl_usuario.id_usuario = :id_usuario;
+                    ";
             //se prepara el sql
             $ejecutar=$conexion->prepare($sql);
             //relacionamos las variables con los registros del sql
-            $ejecutar->bindParam(":nombre_usuario", $nombre_usuario);
-            $ejecutar->bindParam(":email_usuario", $email_usuario);
-            $ejecutar->bindParam(":pass_usuario", $pass_usuario);
-            $ejecutar->bindParam(":fecha_registro", $fecha_registro);
-            //$ejecutar->execute(array('nombre_usuario'=> $usuario->getNombreUsuario(),'pass_usuario'=> $usuario->getPassUsuario(),'email_usuario'=> $usuario->getEmailUsuario() ));
-            //ejecutamos la sentencia sql
+            $ejecutar->bindParam(":apellidos_usuario", $apellidos_usuario);
+            $ejecutar->bindParam(":sexo_usuario", $sexo_usuario);
+            $ejecutar->bindParam(":altura_usuario", $altura_usuario);
+            $ejecutar->bindParam(":fecha_nacimiento", $fecha_nacimiento);
+            //ejecutamos la consulta
             $ejecutar->execute();
+            
+            //y a continuación tenemos que insertar el historial de peso.
             //$resultado = $ejecutar->fetchAll();
             
         } catch (Exception $ex) {
