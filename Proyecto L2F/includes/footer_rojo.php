fd589fc30674cd7a4f62a971aa278a368a6b5b85
Eric Petit <9199.joan23@fje.edu>
2017-05-17 17:59:08 +0200

    header y footers

---
+++ Proyecto L2F/includes/footer_rojo.php
@@ -0,0 +1,33 @@
+ <footer>
+    <div class="container text-center">
+      <div class="row">        
+        <div class="col-md-4">
+             <span class="copyright">Copyright &copy; L2F 2017</span>
+          </div>
+          <div class="col-md-4">
+            <ul class="list-inline social-buttons">
+             <li><a href=""><img src="media/img/social/twitter2.png" width="42px" align="42px" ></a></li>
+             <li><a href=""><img src="media/img/social/face2.png" width="42px" align="42px"></i></a></li>
+             <li><a href=""><img src="media/img/social/insta2.png" width="42px" align="42px"></i></a></li>
+            </ul>
+          </div>
+          <div class="col-md-4">
+            <ul class="list-inline quicklinks">
+              <li><a >Contacto</a></li>
+              <li><a >Ayuda</a></li>
+            </ul>
+          </div>
+        </div>
+        </div>
+</footer>
+  <!-- Finish Footer -->
+  <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
--- Proyecto L2F/main.php
+++ Proyecto L2F/main.php
@@ -37,7 +37,7 @@
                     </div>
                     <div class="panel-body">
                         <h4>Perfil</h4>
-                        <p>Accede a tu historial de pesos y medidas o modifica tus datos de registro.</p>
+                        <p>Accede a tu historial de pesos y medidas o modifica tus datos de registro.</p><br>
                         <a href="#" class="btn btn-primary">Learn More</a>
                     </div>
                 </div>
@@ -90,7 +90,7 @@
                     <div class="panel-body">
                         <h4>Servicios</h4>
                         <p>La personalización acelera los resultados<br/>
-                        ¡Contrata a nuestros especialistas!</p>
+                        ¡Contrata a nuestros especialistas!</p><br>
                         <a href="#" class="btn btn-primary">Learn More</a>
                     </div>
                 </div>
@@ -99,42 +99,13 @@
         </a>
 </div>
 
-   <footer>
-    <div class="container text-center">
-      <div class="row">        
-        <div class="col-md-4">
-             <span class="copyright">Copyright &copy; L2F 2017</span>
-          </div>
-          <div class="col-md-4">
-            <ul class="list-inline social-buttons">
-             <li><a href=""><img src="media/img/social/twitter.png" width="42px" align="42px" ></a></li>
-             <li><a href=""><img src="media/img/social/face.png" width="42px" align="42px"></i></a></li>
-             <li><a href=""><img src="media/img/social/insta.png" width="42px" align="42px"></i></a></li>
-            </ul>
-          </div>
-          <div class="col-md-4">
-            <ul class="list-inline quicklinks">
-              <li><a >Contacto</a></li>
-              <li><a >Ayuda</a></li>
-            </ul>
-          </div>
-        </div>
-        </div>
-      </footer>
-      <!-- Finish Footer -->
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="js/ie10-viewport-bug-workaround.js"></script>
-  </body>
-</html>
+  
 <!-- Conflicto HERE-->
 <?php
+
+require_once("includes/footer_azul.php");
+
+
 /*
 session_start();
 include_once('conexio.php');
@@ -162,3 +133,4 @@ $resultado=mysqli_query($conexion, $sql);
 
 </body>
 </html>*/
+?>
