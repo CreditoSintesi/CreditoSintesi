584e9bfe670c33295dae39f8b1683e5fc3f5baf7
MarcPetit92 <1001489.joan23@fje.edu>
2017-05-19 16:50:02 +0200

    actualizo rutinas

--- Proyecto L2F/user_rutinas.php
+++ Proyecto L2F/user_rutinas.php
@@ -74,12 +74,15 @@ $fecha_actual = date('Y-m-d');
 
 $sql = "SELECT * FROM tbl_rutina_usuario WHERE id_usuario = $id_usuario";
 $resultado = mysqli_query($conexion, $sql) or die (mysqli_error());
+//si el usuario tiene asignada alguna rutina habrá datos
 if(mysqli_num_rows($resultado)>0){
-
+	// echo "esta el usuario";
 	$sql2 = "SELECT DISTINCT * FROM tbl_rutina_usuario , tbl_rutina, tbl_usuario WHERE tbl_rutina.id_rutina = tbl_rutina_usuario.id_rutina  AND tbl_usuario.id_usuario = tbl_rutina_usuario.id_usuario AND fecha_fin > $fecha_actual GROUP BY tbl_rutina.nombre_rutina ";
 
 	//cho $sql2 ."<br>";
-	$resultado = mysqli_query($conexion, $sql2) or die (mysqli_error());	
+	$resultado = mysqli_query($conexion, $sql2) or die (mysqli_error());
+
+	//ahora mostraremos los detalles de la rutina que tiene asignada	
 	if(mysqli_num_rows($resultado)>0){
 
 		while($fila = mysqli_fetch_array($resultado)){
@@ -88,30 +91,34 @@ if(mysqli_num_rows($resultado)>0){
       <div class="col-sm-11">
         <div class="panel panel-primary">
 <?php
+			//echo "rutinas";
+			$id_rutina = $fila['id_rutina'];
 			echo "<h4>".$fila['nombre_rutina']."</h4>";	
 
 			echo " <div class='panel-primary panel-heading' onclick='mostrarInfo(".$fila['id_rutina'].")'> Detalle rutina </div>";
             ?>
                  <div class="panel-primary alert-danger text-center" id='flecha' onclick='esconder()'><img id='up' src='media/img/icon/up.png' height="50" width="50" ></div>
 <?php			echo "<div id='datos'></div>";
+			}
 
 			//aqui vendrá la consulta en la cual se mirará que en la tbl_historial_rutina no esté el id_usuario, id_rutina y la fecha actual para evitar que vuelva a hacer ejercicio hoy. se le mandará un alert o algo... 
-
+	
 			$fecha_actual = date('Y-m-d');
 			$sql = "SELECT * FROM tbl_historial_rutinas WHERE id_usuario = $id_usuario AND id_rutina= $id_rutina AND fecha = '$fecha_actual' ";
-
+			//echo $sql;
+			$resultado = mysqli_query($conexion, $sql) or die (mysqli_error());
 			if(mysqli_num_rows($resultado)>0){
 				echo "<div class='panel-primary panel-heading' style='background-color: grey; color: white;'>Empezar rutina</div></div>";
-			
-		}else{
-			echo "<a href='rutina_diaria.php?id_rutina=".$fila['id_rutina']."'> <div class='panel-primary panel-heading' style='background-color: #F00808; color: white;'>Empezar rutina</div></a></div>";
-		}
+				
+			}else{
+				echo "<a href='rutina_diaria.php?id_rutina=".$fila['id_rutina']."'> <div class='panel-primary panel-heading' style='background-color: #F00808; color: white;'>Empezar rutina</div></a></div>";
+			}
 
 		}
-	}
+	
 }else{
 
-
+//aqui es cuando el usuario no tiene ninguna rutina asignada y tiene que elegir una.
 
 ?>
 
