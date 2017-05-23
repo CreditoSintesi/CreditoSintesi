db1cae94e7013ee4507053ee93f5af3aef0bcf73
Eric Petit <9199.joan23@fje.edu>
2017-05-23 19:02:30 +0200

    + ejer

--- Proyecto L2F/sql_rutinas.sql
+++ Proyecto L2F/sql_rutinas.sql
@@ -30,7 +30,62 @@ VALUES ('11', 'Elevaciones de Piernas - Con Carga', '1', '\\Media\\rutinas\\11.g
 
 VALUES ('12', 'Encogimientos - Con Carga', '1', '\\Media\\rutinas\\12.gif', 'Sujeta una mancuerna con una mano, al costado de tu cuerpo.Inclina la parte superior de tu cuerpo hacia el costado en el que sostienes la mancuerna, y luego de una breve pausa, vuelve a la posición inicial. Completa tu serie y cambia de lado. Ten cuidado de no inclinar demasiado profundamente tu cuerpo hacia los costados, desde donde puede resultar dificultoso volver a enderezarte.');
 
--- tipo 
+-- tipo hombro --
+
+VALUES ('13', 'Prensa de Hombros, Palmas Hacia Adentro', '1', '\\Media\\rutinas\\13.gif', 'Ponte de pie y sujeta dos mancuernas al nivel de los hombros, con las palmas apuntando una hacia la otra. Empuja las mancuernas en forma recta hacia arriba, hasta que tus codos estén cerca de trabarse y bájalas luego de una breve pausa. Ten cuidado de NO mover tu espalda en el intento de ayudar a levantar las mancuernas.');
+
+VALUES ('14', 'Prensa de Hombros, Palmas Hacia Adentro – Con Respaldo', '1', '\\Media\\rutinas\\14.gif', 'Siéntate sobre un banco (o silla) y sujeta dos mancuernas al nivel de los hombros, con las palmas apuntando una hacia la otra. Empuja las mancuernas en forma recta hacia arriba, hasta que tus codos estén cerca de trabarse y bájalas luego de una breve pausa. Ten cuidado de NO mover tu espalda en el intento de ayudar a levantar las mancuernas.');
+
+VALUES ('15', 'Prensa de Hombros, Palmas Hacia Adentro, Alternadas', '1', '\\Media\\rutinas\\15.gif', 'Ponte de pie y sujeta dos mancuernas, una al nivel de tus hombros y la otra más alto con tu brazo extendido, con las palmas apuntando una hacia la otra. Empuja una mancuerna en forma recta hacia arriba, hasta que tu codo esté cerca de trabarse y bájala luego de una breve pausa. Alterna las manos. Ten cuidado de NO mover tu espalda en el intento de ayudar a levantar las mancuernas.');
+
+
+VALUES ('16', 'Prensa de Hombros, Palmas Hacia Adentro, Alternadas, Sentado', '1', '\\Media\\rutinas\\16.gif', 'Siéntate sobre un banco y sujeta dos mancuernas, una al nivel de tus hombros y la otra más alto con tu brazo extendido, con las palmas apuntando una hacia la otra. Empuja una mancuerna en forma recta hacia arriba, hasta que tu codo esté cerca de trabarse y bájala luego de una breve pausa. Alterna las manos. Ten cuidado de NO mover tu espalda en el intento de ayudar a levantar las mancuernas.');
+
+
+VALUES ('17', 'Prensa de Hombros', '1', '\\Media\\rutinas\\17.gif', 'Ponte de pie y sujeta dos mancuernas cerca de tus hombros, con las palmas apuntando hacia adelante. Empuja las mancuernas en forma recta hacia arriba, hasta que tus codos estén cerca de trabarse y bájalas luego de una breve pausa. Ten cuidado de NO mover tu espalda en el intento de ayudar a levantar las mancuernas.');
+
+
+VALUES ('18', 'Prensa de Hombros Sentado', '1', '\\Media\\rutinas\\18.gif', 'Siéntate sobre un banco y sujeta dos mancuernas a la altura de tus hombros, con las palmas apuntando hacia adelante. Empuja las mancuernas en forma recta hacia arriba, hasta que tus codos estén cerca de trabarse y bájalas luego de una breve pausa. Ten cuidado de NO mover tu espalda en el intento de ayudar a levantar las mancuernas.');
+
+
+VALUES ('19', 'Prensa de Hombros con Respaldo', '1', '\\Media\\rutinas\\19.gif', 'Siéntate sobre un banco con respaldo y sujeta dos mancuernas a la altura de tus hombros, con las palmas apuntando una hacia otra. Empuja las mancuernas en forma recta hacia arriba, hasta que tus codos estén cerca de trabarse y bájalas luego de una breve pausa. Ten cuidado de NO mover tu espalda en el intento de ayudar a levantar las mancuernas.');
+
+
+VALUES ('20', 'Vuelos Laterales', '1', '\\Media\\rutinas\\20.gif', 'Ponte de pie y sujeta una mancuerna con cada mano frente a tus caderas, con las palmas apuntando una hacia otra. Eleva las mancuernas hacia los costados, hasta que tus brazos estén cerca de quedar paralelos al suelo y bájalas luego de una breve pausa. Intenta mantener los ángulos en tus codos durante todo el movimiento.');
+
+VALUES ('21', 'Vuelos para Deltoides Inclinados, A Un Brazo', '1', '\\Media\\rutinas\\21.gif', 'Inclínate hacia abajo y sujeta una mancuerna con una mano entre tus piernas, y las rodillas ligeramente dobladas. Eleva la mancuerna hacia el costado hasta que tu brazo esté paralelo al piso y bájala lentamente luego de una breve pausa. Mantén tu espalda recta a lo largo del movimiento.');
+
+
+VALUES ('22', 'Vuelos Laterales, Sentado', '1', '\\Media\\rutinas\\22.gif', 'Siéntate sobre uno de los extremos del banco y sujeta una mancuerna con cada mano, hacia abajo y a los costados de tu cuerpo. Eleva las mancuernas hacia los costados, hasta que tus brazos estén cerca de quedar paralelos al suelo y bájalas lentamente luego de una breve pausa. Mantén tu espalda recta a lo largo del movimiento. ');
+
+
+VALUES ('23', 'Vuelos para Deltoides Posteriores - Inclinado', '1', '\\Media\\rutinas\\23.gif', 'Siéntate sobre uno de los extremos del banco y baja tu cuerpo para coger una mancuerna en cada mano, justo sobre el suelo. Eleva ambas mancuernas hacia tus costados hasta que tus brazos estén casi paralelos al suelo y bájalas lentamente luego de una breve pausa. Mantén el mismo arco pequeño en tus codos durante todo el movimiento.');
+
+
+VALUES ('24', 'Vuelos para Deltoides Posteriores', '1', '\\Media\\rutinas\\24.gif', 'Recuéstate sobre un banco elevado (boca abajo) y sujeta una mancuerna en cada mano por debajo de tu pecho, con los brazos ligeramente doblados. Eleva ambas mancuernas hacia los costados hasta que tus brazos estén casi paralelos al suelo y bájalas lentamente luego de una breve pausa. Mantén el mismo arco pequeño en tus codos durante todo el movimiento.');
+
+
+VALUES ('25', 'Círculos para Deltoides Posteriores', '1', '\\Media\\rutinas\\25.gif', 'Recuéstate sobre tu pecho sobre un banco y sujeta una mancuerna en cada mano a cada lado de tus caderas, con los brazos ligeramente arqueados. Lleva ambas mancuernas hacia adelante hasta que las veas a ambas al mismo tiempo y lentamente llévalas hacia atrás luego de una breve pausa. Mantén el mismo arco pequeño en tus codos y conserva ambas mancuernas a la misma altura desde el piso, durante todo el movimiento. ');
+
+
+VALUES ('26', 'Vuelos para Deltoides Frontales, Brazo Recto', '1', '\\Media\\rutinas\\26.gif', 'Ponte de pie y sujeta una mancuerna en cada mano enfrente de tus muslos. Eleva las mancuernas hacia el nivel de tus hombros y continúa elevándolas hasta tener tus brazos estirados y bájalas lentamente luego de una breve pausa. Mantén tus brazos extendidos durante todo el movimiento.');
+
+
+VALUES ('27', 'Remo Vertical', '1', '\\Media\\rutinas\\27.gif', 'Ponte de pie y sujeta una mancuerna en cada mano enfrente de tus muslos. Levanta ambas mancuernas hasta que tus brazos estén casi paralelos al suelo y bájalas lentamente luego de una breve pausa. Ten cuidado de NO mover tu espalda en el intento de ayudar a levantar las mancuernas.');
+
+
+VALUES ('28', 'Vuelos Frontales', '1', '\\Media\\rutinas\\28.gif', 'Ponte de pie y sujeta una mancuerna con cada mano frente a tus muslos, con las palmas apuntando hacia tu cuerpo. Eleva las mancuernas hacia adelante hasta que tus brazos estén casi paralelos al suelo, y bájalas luego de una breve pausa. Mantén tus brazos extendidos durante todo el movimiento.');
+
+
+VALUES ('29', 'Encogimiento de Hombros', '1', '\\Media\\rutinas\\29.gif', 'Ponte de pie y sujeta una mancuerna con cada mano frente a tus muslos, con las palmas apuntando hacia tu cuerpo. Eleva las mancuernas en forma recta hacia arriba encogiendo tus hombros y bájalas luego de una breve pausa. Mantén tus brazos extendidos durante todo el movimiento.');
+
+
+VALUES ('30', 'Prensa Superior', '1', '\\Media\\rutinas\\30.gif', 'Ponte de pie y sujeta dos mancuernas justo por encima de tus hombros, con las palmas apuntando una hacia otra, y las rodillas arqueadas. Empuja las mancuernas en forma recta hacia arriba hasta que tus brazos estén cerca de trabarse y bájalas lentamente luego de una breve pausa. Exhala al empujar hacia arriba e inhala al bajar las mancuernas.');
+
+
+VALUES ('31', 'Remo para Deltoides Posteriores, Agachado', '1', '\\Media\\rutinas\\31.gif', 'Agáchate flexionando tus caderas y rodillas y coge una mancuerna con cada mano, cerca de tus rodillas, con las palmas apuntando hacia atrás. Eleva las mancuernas hacia arriba en forma recta hasta que tus codos formen un ángulo de 90 grados y bájalas luego de una breve pausa. Intenta concentrarte en sólo mover tus brazos durante el ejercicio.');
+
+
 
 
 
