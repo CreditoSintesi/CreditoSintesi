348cab25d757081a3047f989d09a940acc47aa7b
MarcPetit92 <1001489.joan23@fje.edu>
2017-05-23 15:50:38 +0200

    actualizo

--- Proyecto L2F/finalizar_rutina.proc.php
+++ Proyecto L2F/finalizar_rutina.proc.php
@@ -5,7 +5,7 @@ include_once('conexio.php');
 $id_usuario = $_SESSION['id_usuario'];
 extract($_REQUEST);
 $fecha_actual = date('Y-m-d');
-$sql = "UPDATE tbl_rutina_usuario SET fecha_fin = '$fecha_actual' WHERE id_usuario = $id_usuario AND id_rutina = $id_rutina";
+$sql = "UPDATE tbl_rutina_usuario SET fecha_fin = '$fecha_actual', rutina_finalizada = 'si' WHERE id_usuario = $id_usuario AND id_rutina = $id_rutina";
 
 	//echo $sql ."<br><br><br><br>";
 $resultado = mysqli_query($conexion, $sql) or die (mysqli_error());	
