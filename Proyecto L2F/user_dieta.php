c62497a0cac15f0004965eed0e95606b37de9737
Eric Petit <9199.joan23@fje.edu>
2017-05-24 20:01:21 +0200

    cambios

--- Proyecto L2F/user_dieta.php
+++ Proyecto L2F/user_dieta.php
@@ -1,27 +1,7 @@
 <?php
-session_start();
+
 include_once('conexio.php');
-
-$id_usuario = $_SESSION['id_usuario'];
- $sql = "SELECT * FROM `tbl_usuario` WHERE `id_usuario` = ". $_SESSION['id_usuario'];
-
-    $data_user=mysqli_query($conexion,$sql);
-        while($data = mysqli_fetch_array($data_user))
-             {
-             	if($data['estado_usuario']=="Inactivo")
-             	{
-             		echo "<script type='text/javascript'>alert('¡Ep!¡No has rellenado el cuestionario, nos hacen falta los datos para poder ayudarte a conseguir tus objetivos!');
-					location.href='cuestionario.php?err=2';</script>";
-             		
-             	}
-             	else if($data['estado_usuario']=="Dado de baja")
-             	{echo "<script type='text/javascript'>alert('¡Ep!¡Nos habías abandonado anteriormente!');
-					location.href='index.php?err=2';</script>";
-             	}
-             	 
-                $user_name = $data['nombre_usuario'] ." ". $data['apellidos_usuario'];
-             }
-      require_once("includes/header_verde.php");
+require_once("includes/header_verde.php");
 
 extract($_REQUEST);
 ?>
@@ -96,7 +76,7 @@ echo '</div>';
 }
 
 
-include('includes/footer_rojo.php');
+include('includes/footer_verde.php');
 ?>
 
 
